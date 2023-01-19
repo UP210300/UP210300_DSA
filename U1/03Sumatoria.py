0814cac16326af8356d50fe170ecda4c48fccfd1
UP210300 <up210300@alumnos.upa.edu.mx>
2023-01-18 21:14:36 -0600

    Fixed homework

--- U1/03Sumatoria.py
+++ U1/03Sumatoria.py
@@ -1,11 +1,11 @@
 
 amount = int( input('Enter the amount the money that you have: $'))
 tickets = 0
-counter = 1
+
 
 while (amount > tickets):
     tickets += 1
     amount -= tickets
-    counter += 1
+    
 
 print('The amount of tickets you can get is: ',tickets , 'tickets')
