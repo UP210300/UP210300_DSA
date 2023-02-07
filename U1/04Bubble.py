0440192984c445bd3758e263e25f30bed76b83c0
UP210300 <up210300@alumnos.upa.edu.mx>
2023-02-06 22:46:15 -0600

    Bubble sort method

---
+++ U1/04Bubble.py
@@ -0,0 +1,17 @@
+#Bubble sort method
+
+numbers = [1,8,6,4,3,10,2]
+length = len(numbers)-1
+
+for i in range(length):
+    a = 0
+    b = 1
+    for j in range(length):
+        if numbers[a]>numbers[b]:
+            numbers[a],numbers[b] = numbers[b],numbers[a]
+        a +=1
+        b +=1
+    length -=1
+
+print(numbers)
+
